bf1e0b82c686e80e9a52f2574f923da2b99b8872
Greg Magolan <gmagolan@gmail.com>
2022-04-08 02:54:25 -0700

    @aspect-test/qar@3.0.0

--- npm/qar/bin.js
+++ npm/qar/bin.js
@@ -1,2 +1,3 @@
 #!/usr/bin/env node
 console.log(require('./').id());
+console.log(require('./').data());
--- npm/qar/index.js
+++ npm/qar/index.js
@@ -1,4 +1,5 @@
 const packageJson = require('./package.json')
 module.exports = {
-  id: () => `${packageJson.name}@${packageJson.version}`
+  id: () => `${packageJson.name}@${packageJson.version}`,
+  data: () => require('./data.json')
 }
