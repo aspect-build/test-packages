0a7205b3c2f9885efd3ab441b4575ac74270cc2c
Greg Magolan <gmagolan@gmail.com>
2022-04-22 15:51:39 -0700

    @aspect-test/a & b 2.0.0

--- npm/a/index.js
+++ npm/a/index.js
@@ -1,4 +1,8 @@
 const packageJson = require('./package.json')
+const packageJsonB = require('@aspect-test/b/package.json')
+const packageJsonC = require('@aspect-test/c/package.json')
 module.exports = {
-  id: () => `${packageJson.name}@${packageJson.version}`
+  id: () => `${packageJson.name}@${packageJson.version}`,
+  idB: () => `${packageJsonB.name}@${packageJsonB.version}`,
+  idC: () => `${packageJsonC.name}@${packageJsonC.version}`
 }
--- npm/b/index.js
+++ npm/b/index.js
@@ -1,4 +1,8 @@
 const packageJson = require('./package.json')
+const packageJsonA = require('@aspect-test/a/package.json')
+const packageJsonC = require('@aspect-test/c/package.json')
 module.exports = {
-  id: () => `${packageJson.name}@${packageJson.version}`
+  id: () => `${packageJson.name}@${packageJson.version}`,
+  idA: () => `${packageJsonA.name}@${packageJsonA.version}`,
+  idC: () => `${packageJsonC.name}@${packageJsonC.version}`
 }
