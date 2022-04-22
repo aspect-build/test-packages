b5bc010e5d5940992ff3ecb001c8b2f26f2dd723
Greg Magolan <gmagolan@gmail.com>
2022-04-22 15:18:17 -0700

    use a, b, c packages instead of foo, bar, qar

--- npm/c/bin.js
+++ npm/c/bin.js
@@ -1,3 +1,2 @@
 #!/usr/bin/env node
 console.log(require('./').id());
-console.log(require('./').data());
--- npm/qar/index.js
+++
@@ -1,5 +0,0 @@
-const packageJson = require('./package.json')
-module.exports = {
-  id: () => `${packageJson.name}@${packageJson.version}`,
-  data: () => require('./data.json')
-}
